8f99404e388fa32d046f8c8884e7d49d7f5c8296
Abdulhafiz Muhammed <muhammedabdulhafiz1993@gmail.com>
2024-05-24 22:23:58 +0300

    adding vite and tailwind to the project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ env('APP_NAME')}}</title>
-    @vite(['resourse/css/app.css', 'resourse/js/app.js'])
-</head>
-<body>
-    @if (true)
+@extends('layout')
+
+@section('main')
     <h1>Hello</h1>
-    @else
-        <h1>bye</h1>
-    @endif
-    
-</body>
-</html>
+@endsection
